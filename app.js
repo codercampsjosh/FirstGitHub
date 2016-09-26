79714ed29263c187091eaba97eeb52f8720ab560
Joshua <joshua.christiansen@codercamps.com>
2016-09-26 14:08:06 -0700

    This is the second commit on this repository

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 console.log('hello, world!');
 alert('hey there!');
+console.log('second console log statement');
--- app.ts
+++ app.ts
@@ -1,2 +1,4 @@
 console.log('hello, world!')
 alert('hey there!')
+
+console.log('second console log statement');
